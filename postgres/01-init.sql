bfd666fb0299d79d4c094729d60a5de0845dfda1
paulononato <s4tr14n1@gmail.com>
2025-07-07 14:08:48 -0300

    Update 01-init.sql

--- postgres/01-init.sql
+++ postgres/01-init.sql
@@ -3,5 +3,7 @@ CREATE TABLE IF NOT EXISTS ping_metrics (
     target VARCHAR(255) NOT NULL,
     response_time_ms FLOAT,
     packet_loss_percent FLOAT,
+    http_status_code INTEGER,
+    http_load_time_ms FLOAT,
     created_at TIMESTAMP NOT NULL
 );
